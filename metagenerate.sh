e1d2b7e6ac28c961d68334d1045081877bcdb25e
Sid <shetty.sud@gmail.com>
2014-12-17 14:18:13 -0600

    Added Exif tool cmds

---
+++ metagenerate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# script to get the metadata
+
+# perl /home/sid/Desktop/MS/Image-ExifTool-9.75/exiftool  $1 > /home/sid/Desktop/MS/Data/dev/exifmeta/$2
+
+perl /home/sid/Desktop/MS/Image-ExifTool-9.75/exiftool  $1 > $2
